a840fd836477eca2e5891a8fa927e2d81f76c79b
subodh galande <subodh.dev@outlook.com>
2024-05-15 15:06:03 +0530

    code: Frequency counter pattern & Anagram checker

---
+++ Problem solving patterns/Anagram.js
@@ -0,0 +1,35 @@
+function checkAnagram(str1, str2) {
+  if (str1.length !== str2.length) {
+    console.log("length is not same");
+    return false;
+  }
+
+  let frequencyCounter1 = {};
+  let frequencyCounter2 = {};
+
+  for (let val of str1) {
+    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+  }
+
+  for (let val of str2) {
+    frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+  }
+
+  console.log(frequencyCounter1);
+  console.log(frequencyCounter2);
+
+  for (let key in frequencyCounter1) {
+    if (!(key in frequencyCounter2)) {
+      console.log("matching letters not present in given string");
+      return false;
+    }
+    if (frequencyCounter2[key] !== frequencyCounter1[key]) {
+      console.log(" letters frequency does not match in given string");
+      return false;
+    }
+  }
+  console.log("is anagram");
+  return true;
+}
+
+checkAnagram("anagram", "mraaana");
