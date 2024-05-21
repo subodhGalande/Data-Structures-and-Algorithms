a7716376e66900d71292a346572bd159a3275384
subodh galande <subodh.dev@outlook.com>
2024-05-21 16:27:26 +0530

    fix: linear search code fix to normal for loop

--- Searching Algorithms/simpleLinearSearch.js
+++ Searching Algorithms/simpleLinearSearch.js
@@ -1,13 +1,14 @@
 //simple linear search
 
 linearSearch = (arr, val) => {
-  for (let i of arr) {
+  for (let i = 0; i < arr.length; i++) {
     if (arr[i] === val) {
       console.log(i);
       return i;
     }
   }
+  console.log("value not found");
   return -1;
 };
 
-linearSearch([1, 2, 3, 4, 5, 6], 6);
+linearSearch([1, 2, 3, 4, 5, 6], 1);
