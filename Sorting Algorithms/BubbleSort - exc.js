14331397bc5df1e15371e75d09cc3c4b29ad66f3
subodh galande <subodh.dev@outlook.com>
2024-05-27 17:35:37 +0530

    code: bubble sort naive solution

---
+++ Sorting Algorithms/BubbleSort - exc.js
@@ -0,0 +1,15 @@
+//bubble sort naive solution
+
+bubbleSort = (arr) => {
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  console.log(arr);
+  return arr;
+};
+
+bubbleSort([1, 23, 45, 234, 234, 25, 212]);
