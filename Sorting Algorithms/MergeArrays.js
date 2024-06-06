ae985f1283b370688bd93c19c59823118f7153db
subodh galande <subodh.dev@outlook.com>
2024-06-06 23:31:28 +0530

    code: mergeSort implementation

---
+++ Sorting Algorithms/MergeArrays.js
@@ -0,0 +1,29 @@
+mergeArray = (arr1, arr2) => {
+  let results = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr2[j] > arr1[i]) {
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j]);
+      j++;
+    }
+  }
+
+  while (i < arr1.length) {
+    results.push(arr1[i]);
+    i++;
+  }
+
+  while (j < arr2.length) {
+    results.push(arr2[j]);
+    j++;
+  }
+
+  console.log(results);
+};
+
+mergeArray([67, 78, 89, 90], [1, 2, 3, 4, 5]);
