f910f75be7a46ffe3f340d7445a84a921cca25a4
Subodh Galande <subodh.dsgn@gmail.com>
2024-07-13 19:07:18 +0530

    implt: doubly Linked List - push method

---
+++ Doubly Linked List/doublyLinkedList.js
@@ -0,0 +1,33 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  Push(val) {
+    let newNode = new Node(val);
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+let list = new DoublyLinkedList();
+// list.Push("hey");
+// list.Push("there");
