18f3d382e2351be5b5de073d6bf560a5ef9aa101
Subodh Galande <subodh.dsgn@gmail.com>
2026-01-27 19:01:10 +0530

    chore: lc 701 with recursion

--- Binary Trees/701_Insert_in_BST.js
+++ Binary Trees/701_Insert_in_BST.js
@@ -1,27 +1,45 @@
-var insertIntoBST = function (root, val) {
-  const node = new TreeNode(val);
+//ITERATIVE
 
-  if (root === null) return node;
+// var insertIntoBST = function (root, val) {
+//   const node = new TreeNode(val);
 
-  let curr = root;
+//   if (root === null) return node;
 
-  while (true) {
-    if (val < curr.val) {
-      if (curr.left === null) {
-        curr.left = node;
-        break;
-      }
-      curr = curr.left;
-    } else {
-      if (curr.right === null) {
-        curr.right = node;
-        break;
-      }
-      curr = curr.right;
-    }
+//   let curr = root;
+
+//   while (true) {
+//     if (val < curr.val) {
+//       if (curr.left === null) {
+//         curr.left = node;
+//         break;
+//       }
+//       curr = curr.left;
+//     } else {
+//       if (curr.right === null) {
+//         curr.right = node;
+//         break;
+//       }
+//       curr = curr.right;
+//     }
+//   }
+
+//   return root;
+// };
+
+// RECURSIVE
+
+function insertIntoBST(root, val) {
+  if (root === null) {
+    return new TreeNode(val);
+  }
+
+  if (val < root.val) {
+    root.left = insertIntoBST(root.left, val);
+  } else {
+    root.right = insertIntoBST(root.right, val);
   }
 
   return root;
-};
+}
 
 insertIntoBST([4, 2, 7, 1, 3], 5);
