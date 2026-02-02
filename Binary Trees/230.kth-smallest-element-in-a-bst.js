bc0a5051cda2e50d3992fa990bde530b19a6fdac
Subodh Galande <subodh.dsgn@gmail.com>
2026-02-02 21:51:53 +0530

    chore: 230 kth smallest element in a bst

---
+++ Binary Trees/230.kth-smallest-element-in-a-bst.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=230 lang=javascript
+ *
+ * [230] Kth Smallest Element in a BST
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function (root, k) {
+  let arr = [];
+
+  function inOrder(node) {
+    if (node === null) return;
+    inOrder(node.left);
+    arr.push(node.val);
+    inOrder(node.right);
+  }
+
+  inOrder(root);
+
+  return arr[k - 1];
+};
+// @lc code=end
