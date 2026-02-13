0fbae4efa4fd16d548cfe5ed7939fe32f7dba27b
Subodh Galande <subodh.dsgn@gmail.com>
2026-02-13 16:05:34 +0530

    chore: 112 path sum

---
+++ Binary Search Trees/112.path-sum.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=112 lang=javascript
+ *
+ * [112] Path Sum
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function (root, targetSum) {
+  if (!root) return false;
+
+  if (!root.left && !root.right) {
+    return targetSum === root.val;
+  }
+
+  let remaining = targetSum - root.val;
+
+  return hasPathSum(root.left, remaining) || hasPathSum(root.right, remaining);
+};
+// @lc code=end
