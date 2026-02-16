06462d2a4330beae13df94c54ad98df68a42b298
Subodh Galande <subodh.dsgn@gmail.com>
2026-02-16 17:46:37 +0530

    chore: add minimum depth of binary tree solution

---
+++ Binary Search Trees/111.minimum-depth-of-binary-tree.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=111 lang=javascript
+ *
+ * [111] Minimum Depth of Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function (root) {
+  if (!root) return 0;
+
+  //if leaf
+  if (!root.left && !root.right) return 1;
+
+  //if one side is null, ignore it
+  if (!root.left) return 1 + minDepth(root.right);
+  if (!root.right) return 1 + minDepth(root.left);
+
+  //if both exists
+  return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+};
+// @lc code=end
