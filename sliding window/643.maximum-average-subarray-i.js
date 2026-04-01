2dfa82fe12fb7f75dd632cb55d9d12922af0fcb4
Subodh Galande <subodh.dsgn@gmail.com>
2026-04-01 21:04:59 +0530

    feat: 643 Maximum Average Subarray I problem

---
+++ sliding window/643.maximum-average-subarray-i.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=643 lang=javascript
+ *
+ * [643] Maximum Average Subarray I
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function (nums, k) {
+  let windowSum = 0;
+
+  for (let i = 0; i < k; i++) {
+    windowSum += nums[i];
+  }
+
+  let maxSum = windowSum;
+
+  for (let i = k; i < nums.length; i++) {
+    windowSum += nums[i] - nums[i - k];
+    maxSum = Math.max(maxSum, windowSum);
+  }
+
+  return maxSum / k;
+};
+// @lc code=end
