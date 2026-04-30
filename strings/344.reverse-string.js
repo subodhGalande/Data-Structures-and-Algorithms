c64d368a1461866aed1b8abae62c118c0f534dc1
Subodh Galande <subodh.dsgn@gmail.com>
2026-04-30 21:56:09 +0530

    feat: add solution for 344 Reverse String problem

---
+++ strings/344.reverse-string.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=344 lang=javascript
+ *
+ * [344] Reverse String
+ */
+
+// @lc code=start
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+  let l = 0;
+  let r = s.length - 1;
+
+  while (l < r) {
+    [s[l], s[r]] = [s[r], s[l]];
+    l++;
+    r--;
+  }
+  return l;
+};
+// @lc code=end
