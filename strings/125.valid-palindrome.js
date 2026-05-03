8fd5f64f19f0fe273d13375eb9e3d4f25e1f8a5a
Subodh Galande <subodh.dsgn@gmail.com>
2026-05-03 18:27:14 +0530

    feat: 125 valid palindrome

--- strings/125.valid-palindrome.js
+++ strings/125.valid-palindrome.js
@@ -10,8 +10,17 @@
  * @return {boolean}
  */
 var isPalindrome = function (s) {
-  let clean = s.toLowerCase().replace(/[^a-z0-9]/g, "");
-  let reversed = clean.split("").reverse().join("");
-  return clean === reversed;
+  let str = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+  let l = 0;
+  let r = str.length - 1;
+
+  while (l < r) {
+    if (str[l] !== str[r]) return false;
+    l++;
+    r--;
+  }
+
+  return true;
 };
 // @lc code=end
