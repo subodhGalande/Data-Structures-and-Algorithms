999d6ed8f7b6d62405ebb7d3aaa42c4ea5b961ed
Subodh Galande <subodh.dsgn@gmail.com>
2026-06-06 20:33:29 +0530

    feat: add solution for 122 Best Time to Buy and Sell Stock II problem

---
+++ Arrays/122.best-time-to-buy-and-sell-stock-ii.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=122 lang=javascript
+ *
+ * [122] Best Time to Buy and Sell Stock II
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  let profit = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] > prices[i - 1]) {
+      profit += prices[i] - prices[i - 1];
+    }
+  }
+
+  return profit;
+};
+// @lc code=end
